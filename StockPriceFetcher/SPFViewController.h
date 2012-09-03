7ea2d3fef8ae460a5c8033072727598e026ea65e
Justin Driscoll <justin@driscolldev.com>
2012-09-03 10:35:54 -0400

    Initial Commit

---
+++ StockPriceFetcher/SPFViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPFViewController.h
+//  StockPriceFetcher
+//
+//  Created by Justin Driscoll on 9/3/12.
+//  Copyright (c) 2012 Justin Driscoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPFViewController : UIViewController
+
+@end
---
+++ StockPriceFetcher/SPFViewController.m
@@ -0,0 +1,29 @@
+//
+//  SPFViewController.m
+//  StockPriceFetcher
+//
+//  Created by Justin Driscoll on 9/3/12.
+//  Copyright (c) 2012 Justin Driscoll. All rights reserved.
+//
+
+#import "SPFViewController.h"
+
+@interface SPFViewController ()
+
+@end
+
+@implementation SPFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
